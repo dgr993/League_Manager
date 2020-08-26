8797213e9d189f33ee0497bbc71ba5b316d5e943
Wuzup <dgr993@gmail.com>
2020-08-25 21:42:17 -0600

    mvc created and league_manager.js created with the tables and connection.js info added

---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the league_manager_db if it exists currently --
+DROP DATABASE IF EXISTS league_manager_db;
+-- Creates the "league_manager_db" database --
+CREATE DATABASE league_manager_db;
