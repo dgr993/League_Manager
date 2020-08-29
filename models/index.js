96a15854b9be90f05375985dec8f7455db5a89e9
Wuzup <dgr993@gmail.com>
2020-08-29 13:21:55 -0600

    hid password

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+require("dotenv").config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -8,11 +8,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, process.env.password || config.password, config);
 }
 
 fs
