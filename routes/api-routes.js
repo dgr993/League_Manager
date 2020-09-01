6ddb2fb890268cf0f4abb44c17672dcc41d07930
Wuzup <dgr993@gmail.com>
2020-09-01 17:16:51 -0600

    added res.render to html file
    :

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,14 +6,8 @@ module.exports = function (app) {
     // Using the passport.authenticate middleware with our local strategy.
     // If the user has valid login credentials, send them to the members page.
     // Otherwise the user will be sent an error
-    app.get("/", (req, res) => {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.render("members");
-        }
-        res.render("signup");
-    });
-    
+  
+
     app.post("/api/login", passport.authenticate("local"), (req, res) => {
         // Sending back a password, even a hashed password, isn't a good idea
         res.json({
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,10 +8,11 @@ module.exports = function (app) {
     app.get("/", (req, res) => {
         // If the user already has an account send them to the members page
         if (req.user) {
-            res.redirect("/members");
+            res.render("members");
         }
-        res.sendFile(path.join(__dirname, "../public/signup.html"));
+        res.render("signup");
     });
+
 
     app.get("/login", (req, res) => {
         // If the user already has an account send them to the members page
