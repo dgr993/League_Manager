e00e2b6b3549e3f53da4937f32d104952e90ac7d
Wuzup <dgr993@gmail.com>
2020-09-01 19:42:26 -0600

    added index.js model and code to hide password :

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 'use strict';
-require("dotenv").config();
 
 const fs = require('fs');
 const path = require('path');
@@ -8,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
+require('dotenv').config();
 
 let sequelize;
 if (config.use_env_variable) {
