9926109d35c641fe31a2dfca5e081211cdde26a5
Trevor Otterson <totterson97@gmail.com>
2020-09-03 18:24:21 -0600

    pulling new info

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
     app.get("/", (req, res) => {
         // If the user already has an account send them to the members page
         if (req.user) {
-            res.render("homepage",  { layout: 'loggedin' });
+            return res.render("homepage",  { layout: 'loggedin' });
         }
         res.render("signup");
     });
@@ -17,7 +17,7 @@ module.exports = function (app) {
     app.get("/login", (req, res) => {
         // If the user already has an account send them to the members page
         if (req.user) {
-            res.redirect("/");
+            return res.redirect("/");
         }
         res.render("login");
     });
