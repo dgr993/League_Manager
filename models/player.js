94bd5f3644ca96c43cb7441e32211ab5b69f5160
talmage <talmagelucas@gmail.com>
2020-09-03 20:30:24 -0600

    merge to master

--- models/player.js
+++ models/player.js
@@ -1,39 +1,31 @@
 module.exports = function (sequelize, DataTypes) {
-    var Player = sequelize.define("Player", {
-      playerName: DataTypes.STRING,
-      playerAge: DataTypes.INTEGER,
+  var Player = sequelize.define("Player", {
+    playerName: DataTypes.STRING,
+    playerAge: DataTypes.INTEGER,
 
 
-    });
-
-    Player.associate = function(models) {
-      // We're saying that a Team should belong to an GameHistory
-      // A Post can't be created without an GameHistory due to the foreign key constraint
-      Player.belongsTo(models.Coach, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-
-<<<<<<< HEAD
   });
+
+  Player.associate = function (models) {
+    // We're saying that a Team should belong to an GameHistory
+    // A Post can't be created without an GameHistory due to the foreign key constraint
+    Player.belongsTo(models.Coach, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  Player.associate = function (models) {
+    // We're saying that a Team should belong to an GameHistory
+    // A Post can't be created without an GameHistory due to the foreign key constraint
+    Player.belongsTo(models.Team, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
 
   return Player;
 };
-
-=======
-    Player.associate = function(models) {
-      // We're saying that a Team should belong to an GameHistory
-      // A Post can't be created without an GameHistory due to the foreign key constraint
-      Player.belongsTo(models.Team, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-
-
-    return Player;
-  };
->>>>>>> master
