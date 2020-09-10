845cc4ae80003ac524a47b75cd41dc6030bff57e
Dylan Dewey <dylandewey41@gmail.com>
2020-09-09 19:05:17 -0600

    merge to master

--- models/team.js
+++ models/team.js
@@ -1,39 +1,51 @@
 module.exports = function (sequelize, DataTypes) {
-    var Team = sequelize.define("Team", {
-  
-      teamName: DataTypes.STRING,
-      teamWins: DataTypes.INTEGER,
-      teamLosses: DataTypes.INTEGER,
-      teamCoach: DataTypes.STRING,  
-    });
+  var Team = sequelize.define("Team", {
 
-    Team.associate = function(models) {
-      // Associating Author with Posts
-      // When an Author is deleted, also delete any associated Posts
-      Team.hasMany(models.GameHistory, {
-        onDelete: "cascade"
-      });
-    };
+    teamName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // teamWins: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+    // teamLosses: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+    teamCoach: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  });
 
-    Team.associate = function(models) {
-      // We're saying that a Team should belong to an GameHistory
-      // A Post can't be created without an GameHistory due to the foreign key constraint
-      Team.belongsTo(models.Coach, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
+  // Team.associate = function (models) {
+  //   // Associating Author with Posts
+  //   // When an Author is deleted, also delete any associated Posts
+  //   Team.hasMany(models.GameHistory, {
+  //     onDelete: "cascade"
+  //   });
+  // };
 
-    Team.associate = function(models) {
-      // We're saying that a Team should belong to an GameHistory
-      // A Post can't be created without an GameHistory due to the foreign key constraint
-      Team.belongsTo(models.League, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-    
-    return Team;
-  };
+  // Team.associate = function (models) {
+  //   // We're saying that a Team should belong to an GameHistory
+  //   // A Post can't be created without an GameHistory due to the foreign key constraint
+  //   Team.belongsTo(models.Coach, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
+
+  // Team.associate = function (models) {
+  //   // We're saying that a Team should belong to an GameHistory
+  //   // A Post can't be created without an GameHistory due to the foreign key constraint
+  //   Team.belongsTo(models.League, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
+
+  return Team;
+};
--- public/assets/js/newteam.js
+++ public/assets/js/newteam.js
@@ -1,35 +1,35 @@
 $(document).ready(() => {
     // Getting references to our form and input
-    const createNew = $("form.player");
-    const playerInput = $("#name-input");
-    const ageInput = $("#age-input");
+    const createNew = $("form.team");
+    const teamInput = $("#team-input");
+    const coachInput = $("#coach-input");
 
     // When the create button is clicked, we validate the email and password are not blank
     createNew.on("submit", event => {
         event.preventDefault();
         const userData = {
-            playerName: playerInput.val().trim(),
-            playerAge: ageInput.val().trim()
+            teamName: teamInput.val().trim(),
+            coach: coachInput.val().trim(),
         };
         console.log(userData);
-        if (!userData.playerName || !userData.playerAge) {
+        if (!userData.teamName || !userData.teamCoach) {
             return;
         }
         // If we have an email and password, run the signUpUser function
-        createNewPlayer(userData.playerName, userData.playerAge);
-        playerInput.val("");
-        ageInput.val("");
+        createNewTeam(userData.teamName, userData.teamCoach);
+        teamInput.val("");
+        coachInput.val("");
     });
 
     // Does a post to the signup route. If successful, we are redirected to the members page
     // Otherwise we log any errors
-    function createNewPlayer(playerName, playerAge) {
-        $.post("/api/players", {
-            playerName: playerName,
-            playerAge: playerAge
+    function createNewTeam(teamName, teamCoach) {
+        $.post("/api/teams", {
+            teamName: teamName,
+            teamCoach: teamCoach
         })
             .then(() => {
-                window.location.replace("/players");
+                window.location.replace("/teams");
                 // If there's an error, handle it by throwing up a bootstrap alert
             })
             .catch(err => {
