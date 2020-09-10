4c8b7c7aabb9a5d670a9e1fa359994da1be32f16
Wuzup <dgr993@gmail.com>
2020-09-10 12:45:24 -0600

    merge

--- public/assets/js/navteam.js
+++ public/assets/js/navteam.js
@@ -9,3 +9,11 @@ $(document).ready(() => {
             playerName: playerName,
             playerAge: playerAge
         })
+
+        router.get("/burgers", function(_req_, _res_) {
+ // express callback response by calling burger.selectAllBurger
+ burger.all(function(_burgerData_) {
+   // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+   _res_.render("index", { burger_data: _burgerData_ });
+ });
+});
