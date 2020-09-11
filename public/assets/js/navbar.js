8e21529563412290840df38b207736256d33d86b
Wuzup <dgr993@gmail.com>
2020-09-10 19:13:39 -0600

    removed head and master codes lines added by merging

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -1,19 +1,12 @@
-
-
-
 function getTeams() {
     $.get("/api/teams", function (data) {
         data.map(team => {
             $("#teamDrop").append(
-                
-<<<<<<< HEAD
                     `<li><a href=/teams/${team.id}>${team.teamName}</a></li>`
-=======
-                    `<li><a href=teams/${team.id}>${team.teamName}</a></li>`
-               
+
                     //"<button type='button' class='btn btn - danger'>Danger</button>",
                     
->>>>>>> fe34f5fec44dd2735f420ba75913e05e37edf043
+
                 
 
 
@@ -23,26 +16,11 @@ function getTeams() {
     });
 }
 
-
-
-
-
-
-
-
 function getPlayers() {
     $.get("/api/players", function (data) {
         data.map(player => {
             $("#playersDrop").append(
-<<<<<<< HEAD
-                
-                    `<li><a href=/players/${player.id}>${player.playerName}</a></li>`
-=======
-
                     `<li><a href=teams/${player.id}>${player.playerName}</a></li>`
->>>>>>> master
-                
-
             )
 
         })
@@ -53,17 +31,8 @@ function getLeagues() {
     $.get("/api/leagues", function (data) {
         console.log(data)
         data.map(league => {
-            $("#leaguesDrop").append(
-<<<<<<< HEAD
-                
+            $("#leaguesDrop").append(   
                     `<li><a href=/leagues/${league.id}>${league.leagueName}</a></li>`
-=======
-
- 
-                    `<li><a href=teams/${league.id}>${league.leagueName}</a></li>`
->>>>>>> master
-                
-
             )
 
         })
